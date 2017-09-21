9418bc5bc14985449895f848e68fee0d7a116df5
sandeep.kumar <sandeepvish3006@gmail.com>
2017-09-21 23:17:36 +0530

    added new files

---
+++ springboot/src/main/java/com/pepcus/springboot/BootApplication.java
@@ -0,0 +1,13 @@
+package com.pepcus.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(BootApplication.class, args);
+  }
+
+}
---
+++ springboot/src/main/java/com/pepcus/springboot/Test.java
@@ -0,0 +1,10 @@
+package com.pepcus.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+public class Test {
+	public static void main(String[] args) throws Exception {
+		System.out.println("fbgftht");
+	}
+
+}
